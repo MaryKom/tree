a1e18e3d49b4e0e898931813ef5c772bff53900b
Maria Komendantova <masha.komendantova@yandex.ru>
2023-06-21 11:29:17 +0300

    Initial commit

---
+++ src/Node.java
@@ -0,0 +1,10 @@
+public class Node {
+    public int iData;
+    public double dData;
+    public Node leftChild;
+    public Node rightChild;
+
+    public void displayNode() {
+        System.out.print("{" + iData + ". " + dData+"} ");
+    }
+}
